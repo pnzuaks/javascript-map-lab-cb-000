4309c499422f9ae393d867d6ff0d05b7ab974bf8
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-22 23:33:36 +0000

    Done.

--- index.js
+++ index.js
@@ -11,9 +11,22 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(function(robot) {
+  if (knownDecepticons.includes(robot.name)) {
+    return Object.assign({}, robot, {
+      alliance: 'decepticon'
+    })
+  } else {
+    return Object.assign({}, robot, {
+      alliance: 'autobot'
+    })
+  }
+})
+
+
 const zebraStripes = [
   { width: 9.12, color: null },
-  { width: 5.71, color: null },
+  { width: 5.74, color: null },
   { width: 6.01, color: null },
   { width: 1.54, color: null },
   { width: 8.34, color: null },
@@ -21,3 +34,19 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+
+const coloredZebraStripes = zebraStripes.map(function(zebra) {
+  var lastString = zebra.width.toString().slice(-1)
+  var lastDigit = parseInt(lastString, 10)
+
+  if (lastDigit % 2 == 0) {
+    return Object.assign({},zebra, {
+      color: "black"
+    })
+  } else {
+    return Object.assign({},zebra, {
+      color: "white"
+    })
+  }
+})
