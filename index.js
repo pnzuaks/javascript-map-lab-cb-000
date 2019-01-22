c22e1de259b989afd55af2cc386f14ec62ca23c5
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-22 22:35:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const sortedRobots = []
 
 robots.map(function(robot) {
   if (knownDecepticons.includes(robot)) {
-    return Object.assign({}, robot, {
+    sortedRobots.push(Object.assign({}, robot, {
       alliance: 'decepticon'
-    })
+    }))
   } else {
     return Object.assign({}, robot, {
       alliance: 'alliance'
