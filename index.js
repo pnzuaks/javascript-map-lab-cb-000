a6e3217f4ada573100e923e4dd1b72946e12d2c0
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-22 22:47:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const sortedRobots = robots.map(function(robot) {
 })
 
 
-
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -35,3 +34,7 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes = zebraStripes.map(function(zebra) {
+  
+})
